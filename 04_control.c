492d7622928bad9d42ef8ff4c119e5ae1d8bccaa
tjrwldnjs <sue06024@naver.com>
2018-10-11 15:35:13 +0900

    2nd

--- 04_control.c
+++ 04_control.c
@@ -10,20 +10,14 @@ int main(int argc, char *argv[]) {
 	scanf("%d", &i);
 	
 	
-	
 	if (i>0)
-	{	printf("양수입니다");}
-		
-		
-		
-	else if (i==0)	
-	{	printf("0입니다");}
-	
+	{	printf("절댓값은 %i 입니다.", i);
+	}
 	
 	
 	else 
-	{	printf("음수입니다");}
-		 
+	{	printf("절댓값은 %i 입니다.", -i);
+	} 
 	
 	
 	
