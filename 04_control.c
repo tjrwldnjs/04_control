0e10a04cff6204873cec54038c10e47204a989df
tjrwldnjs <sue06024@naver.com>
2018-10-11 15:23:44 +0900

    initial submission

---
+++ 04_control.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i;
+	
+	printf("정수 하나를 입력하시오\n");
+	scanf("%d", &i);
+	
+	
+	
+	if (i>0)
+	{	printf("양수입니다");}
+		
+		
+		
+	else if (i==0)	
+	{	printf("0입니다");}
+	
+	
+	
+	else 
+	{	printf("음수입니다");}
+		 
+	
+	
+	
+	
+	
+	return 0;
+}
